e2d040b20368392f704b087e1f6aa04f4b0f1d04
Benjamin Inemugha <bennyblasco@gmail.com>
2020-07-04 21:29:18 +0100

    Initial Commit

---
+++ Shake Me!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shake Me!
+//
+//  Created by Benjamin Inemugha on 04/07/2020.
+//  Copyright © 2020 Techelopers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
